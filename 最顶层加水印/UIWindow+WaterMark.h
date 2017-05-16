139bbcc3d4d7e61b7337e571523d3a5d18771bb4
刘伟东 <EX-LIUWEIDONG001@pingan.com.cn>
2017-05-16 09:29:43 +0800

    first commit

---
+++ 最顶层加水印/UIWindow+WaterMark.h
@@ -0,0 +1,13 @@
+//
+//  UIWindow+WaterMark.h
+//  最顶层加水印
+//
+//  Created by weidong liu on 2017/5/15.
+//  Copyright © 2017年 weidong liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWindow (WaterMark)
+
+@end
---
+++ 最顶层加水印/UIWindow+WaterMark.m
@@ -0,0 +1,13 @@
+//
+//  UIWindow+WaterMark.m
+//  最顶层加水印
+//
+//  Created by weidong liu on 2017/5/15.
+//  Copyright © 2017年 weidong liu. All rights reserved.
+//
+
+#import "UIWindow+WaterMark.h"
+
+@implementation UIWindow (WaterMark)
+
+@end
